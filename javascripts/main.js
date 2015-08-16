530c0fc35a5d628cb88d08c1d2b6f18e7c0d5c9b
mwcz <mwc@clayto.com>
2015-08-16 16:42:02 -0400

    add video gallery

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+var iframe = $('iframe#vimeo-player');
+
+function handle_vid_click() {
+    iframe.attr('src', $(this).find('[data-vid-src]').attr('data-vid-src') + '?autoplay=1');
+}
+
+function init_vimeo_picker() {
+    // get every img with data-vid-src
+    // get ref to iframe
+    // create onclick for each img which sets iframe's src to data-vid-src
+    var vidlinks = $('.vimeo-thumbnail');
+    vidlinks.on('click', handle_vid_click);
+}
+
+init_vimeo_picker();
+
+function set_vimeo_iframe_height() {
+    iframe.attr('height', iframe.width() / (1280/720) );
+}
+
+document.addEventListener('DOMContentLoaded', set_vimeo_iframe_height);
+window.addEventListener('resize', set_vimeo_iframe_height);
