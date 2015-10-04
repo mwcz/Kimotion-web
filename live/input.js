8596c683f6567fd1b732fbe57602a3a93a719c66
mwcz <mwc@clayto.com>
2015-10-04 16:32:25 -0400

    update kimotion

--- live/input.js
+++ live/input.js
@@ -1,4 +1,5 @@
 define(['exports', 'module', 'recording'], function (exports, module, _recording) {
+  // import websockets from 'websockets';
   'use strict';
 
   function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
@@ -6,7 +7,6 @@ define(['exports', 'module', 'recording'], function (exports, module, _recording
   var _recording2 = _interopRequireDefault(_recording);
 
   // var input_source = [websockets, recording][~~confirm('Ok for recording, Cancel for websockets')]();
-
   var input_source = _recording2['default']();
 
   module.exports = input_source;
